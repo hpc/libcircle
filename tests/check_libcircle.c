1418342121b0eeb16f93969f63f24461ac2465c7
Jon Bringhurst <jon@bringhurst.org>
2011-08-19 21:13:53 -0600

    Minor renaming.

--- tests/check_libcircle.c
+++ tests/check_libcircle.c
@@ -4,12 +4,12 @@
 START_TEST (test_context_create)
 {
   CIRCLE_handle *c;
-  c = CIRCLE_create();
+  c = CIRCLE_handle_create();
 
   fail_unless (c != NULL, 
 	       "Circle context was null.");
 
-  CIRCLE_context_free(c);
+  CIRCLE_handle_free(c);
 }
 END_TEST
 
