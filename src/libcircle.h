cfa4e85a0c98c383f777e58b9269ab75699c2642
Jon Bringhurst <jon@bringhurst.org>
2011-08-22 16:37:17 -0600

    Initial commit of public header.

---
+++ src/libcircle.h
@@ -0,0 +1,47 @@
+#ifndef LIBCIRCLE_H
+#define LIBCIRCLE_H
+
+/*
+ * The interface to the work queue. This can be accessed from within the
+ * process and create work callbacks.
+ */
+typedef struct {
+    void (*enqueue)(void *element);
+    void (*dequeue)(void *element);
+} CIRCLE_handle;
+
+/*
+ * The type for defining callbacks for create and process.
+ */
+typedef void (*CIRCLE_cb)(CIRCLE_handle *handle);
+
+/*
+ * Initialize internal state needed by libcircle.
+ */
+void CIRCLE_init(void);
+
+/*
+ * Processing and creating work is done through callbacks. Here's how we tell
+ * libcircle about our function which creates work.
+ */
+void CIRCLE_cb_create(CIRCLE_cb func);
+
+/*
+ * After you give libcircle a way to create work, you need to tell it how that
+ * work should be processed.
+ */
+void CIRCLE_cb_process(CIRCLE_cb func);
+
+/*
+ * Once you've defined and told libcircle about your callbacks, use this to
+ * execute your program.
+ */
+void CIRCLE_begin(void);
+
+/*
+ * After your program has executed, give libcircle a chance to clean up after
+ * itself by calling this.
+ */
+void CIRCLE_finalize(void);
+
+#endif /* LIBCIRCLE_H */
