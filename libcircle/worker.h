48a5ab7b1b6ca286f98b5a4b9e689d7c4cfe7555
Jon Bringhurst <jon@bringhurst.org>
2011-09-29 15:57:05 -0600

    Fixed two missing prototypes.

--- libcircle/worker.h
+++ libcircle/worker.h
@@ -2,7 +2,11 @@
 #define WORKER_H
 
 #include "libcircle.h"
+
 #define LIBCIRCLE_MPI_ERROR 32
+
 int CIRCLE_worker();
+int _CIRCLE_read_restarts();
+int _CIRCLE_checkpoint();
 
 #endif /* WORKER_H */
