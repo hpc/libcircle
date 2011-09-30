10cd136855144ee86fa32fdd3cf66614157bb4bd
Jharrod LaFon <jlafon@lanl.gov>
2011-09-29 22:21:53 -0600

    Cleaned up worker function.  Refactored.

--- libcircle/worker.c
+++ libcircle/worker.c
@@ -53,76 +53,36 @@ _CIRCLE_checkpoint()
     return CIRCLE_queue_write(CIRCLE_INPUT_ST.queue, CIRCLE_global_rank);
 }
 
-int
-CIRCLE_worker()
+void 
+CIRCLE_init_local_state(CIRCLE_state_st * local_state,int size)
+{
+    int i = 0;
+    local_state->token = WHITE;
+    local_state->have_token = 0;
+    local_state->term_flag = 0;
+    local_state->work_flag = 0;
+    local_state->work_pending_request = 0;
+    local_state->request_pending_receive = 0;
+    local_state->term_pending_receive = 0;
+    local_state->incoming_token = BLACK;
+    local_state->request_offsets = (unsigned int*) calloc(CIRCLE_INITIAL_QUEUE_SIZE,sizeof(unsigned int));
+    local_state->work_offsets = (unsigned int*) calloc(CIRCLE_INITIAL_QUEUE_SIZE,sizeof(unsigned int));
+    local_state->mpi_state_st->request_status = (MPI_Status *) malloc(sizeof(MPI_Status)*size);
+    local_state->request_flag = (int *) calloc(size,sizeof(int));
+    local_state->request_recv_buf = (int *) calloc(size,sizeof(int));
+    local_state->mpi_state_st->request_request = (MPI_Request*) malloc(sizeof(MPI_Request)*size);
+    for(i = 0; i < size; i++)
+        local_state->mpi_state_st->request_request[i] = MPI_REQUEST_NULL;
+    local_state->work_request_tries = 0;
+    return;
+}
+
+void
+CIRCLE_work_loop(CIRCLE_state_st * sptr,CIRCLE_handle * queue_handle)
 {
     int token = WHITE;
-    int token_partner;
-    
-    /* Holds all worker state */
-    CIRCLE_state_st local_state;
-    CIRCLE_state_st * sptr = &local_state;
-
-    /* Holds all mpi state */
-    CIRCLE_mpi_state_st mpi_s;
-    local_state.mpi_state_st = &mpi_s;
-
-    /* Holds work elements */
-
-    /* Provides an interface to the queue. */
-    CIRCLE_handle queue_handle;
-    queue_handle.enqueue = &CIRCLE_enqueue;
-    queue_handle.dequeue = &CIRCLE_dequeue;
-    queue_handle.local_queue_size = &CIRCLE_local_queue_size;
-  
-    int rank = -1;
-    int size = -1;
-    int next_processor;
-    int term_status = -1;
     int work_status = -1;
-    int j = 0;
-
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Errhandler circle_err;
-    MPI_Comm_create_errhandler(CIRCLE_MPI_error_handler,&circle_err);
-    MPI_Comm_set_errhandler(MPI_COMM_WORLD,circle_err);
-    
-    srand(rank);
-    rank = CIRCLE_global_rank;
-    local_state.rank = rank;
-    local_state.size = size;
-    local_state.token = WHITE;
-    next_processor = (rank+1) % size;
-    token_partner = (rank-1)%size;
-    local_state.next_processor = (rank+1) % size;
-    local_state.token_partner = (rank-1)%size;
-    if(token_partner < 0) token_partner = size-1;
-   
-    /* Initial local state */
-    local_state.have_token = 0;
-    local_state.term_flag = 0;
-    local_state.work_flag = 0;
-    local_state.work_pending_request = 0;
-    local_state.request_pending_receive = 0;
-    local_state.term_pending_receive = 0;
-    local_state.incoming_token = BLACK;
-    local_state.request_offsets = (unsigned int*) calloc(CIRCLE_INITIAL_QUEUE_SIZE,sizeof(unsigned int));
-    local_state.work_offsets = (unsigned int*) calloc(CIRCLE_INITIAL_QUEUE_SIZE,sizeof(unsigned int));
-    local_state.mpi_state_st->request_status = (MPI_Status *) malloc(sizeof(MPI_Status)*size);
-    int i = 0;
-    local_state.request_flag = (int *) calloc(size,sizeof(int));
-    local_state.request_recv_buf = (int *) calloc(size,sizeof(int));
-    local_state.mpi_state_st->request_request = (MPI_Request*) malloc(sizeof(MPI_Request)*size);
-    for(i = 0; i < size; i++)
-        local_state.mpi_state_st->request_request[i] = MPI_REQUEST_NULL;
-    local_state.work_request_tries = 0;
-    
-    /* Master rank starts out with the initial data creation */
-    if(rank == 0)
-    {
-        (*(CIRCLE_INPUT_ST.create_cb))(&queue_handle);
-        local_state.have_token = 1;
-    }
+    int term_status = -1;
     /* Loop until done */
     while(token != DONE)
     {
@@ -147,7 +107,7 @@ CIRCLE_worker()
         if(CIRCLE_INPUT_ST.queue->count > 0 && !CIRCLE_ABORT_FLAG)
         {
            // LOG(LOG_DBG,"Calling user callback.");
-            (*(CIRCLE_INPUT_ST.process_cb))(&queue_handle);
+            (*(CIRCLE_INPUT_ST.process_cb))(queue_handle);
         }
         /* If I don't have work, check for termination */
         else if(token != DONE)
@@ -161,7 +121,13 @@ CIRCLE_worker()
             }
         }
     }
-
+    return;
+}
+void 
+CIRCLE_cleanup_mpi_messages(CIRCLE_state_st * sptr)
+{
+    int i = 0;
+    int j = 0;
     /* Make sure that all pending work requests are answered. */
     for(j = 0; j < sptr->size; j++)
      for(i = 0; i < sptr->size; i++)
@@ -178,6 +144,56 @@ CIRCLE_worker()
                 MPI_Start(&sptr->mpi_state_st->request_request[i]);
             }
         }
+    return;
+}
+int
+CIRCLE_worker()
+{
+    int token_partner;
+    /* Holds all worker state */
+    CIRCLE_state_st local_state;
+    CIRCLE_state_st * sptr = &local_state;
+
+    /* Holds all mpi state */
+    CIRCLE_mpi_state_st mpi_s;
+    local_state.mpi_state_st = &mpi_s;
+
+    /* Holds work elements */
+
+    /* Provides an interface to the queue. */
+    CIRCLE_handle queue_handle;
+    queue_handle.enqueue = &CIRCLE_enqueue;
+    queue_handle.dequeue = &CIRCLE_dequeue;
+    queue_handle.local_queue_size = &CIRCLE_local_queue_size;
+  
+    int rank = -1;
+    int size = -1;
+    int next_processor;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Errhandler circle_err;
+    MPI_Comm_create_errhandler(CIRCLE_MPI_error_handler,&circle_err);
+    MPI_Comm_set_errhandler(MPI_COMM_WORLD,circle_err);
+    
+    srand(rank);
+    rank = CIRCLE_global_rank;
+    local_state.rank = rank;
+    local_state.size = size;
+    next_processor = (rank+1) % size;
+    token_partner = (rank-1)%size;
+    local_state.next_processor = (rank+1) % size;
+    local_state.token_partner = (rank-1)%size;
+    if(token_partner < 0) token_partner = size-1;
+     /* Initial local state */
+    CIRCLE_init_local_state(sptr,size);
+    /* Master rank starts out with the initial data creation */
+    if(rank == 0)
+    {
+        (*(CIRCLE_INPUT_ST.create_cb))(&queue_handle);
+        local_state.have_token = 1;
+    }
+    CIRCLE_work_loop(sptr,&queue_handle);
+    CIRCLE_cleanup_mpi_messages(sptr);
     if(CIRCLE_ABORT_FLAG)
         CIRCLE_checkpoint();
     MPI_Barrier(MPI_COMM_WORLD);
