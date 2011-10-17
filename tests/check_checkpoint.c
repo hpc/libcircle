c6a7a868b3cb8cb1ba8b227c70528be37a413e43
Jon Bringhurst <jon@bringhurst.org>
2011-10-17 11:07:47 -0600

    Formatted tests.

--- tests/check_checkpoint.c
+++ tests/check_checkpoint.c
@@ -6,40 +6,40 @@
 START_TEST
 (test_checkpoint_single_read_write)
 {
-    char * test_string = "Here's a test string!";
+    char* test_string = "Here's a test string!";
     char result[CIRCLE_MAX_STRING_LEN];
-	int fakerank = 5;
+    int fakerank = 5;
 
-	int checkpoint_write_ret = -1;
-	int checkpoint_read_ret = -1;
+    int checkpoint_write_ret = -1;
+    int checkpoint_read_ret = -1;
 
     CIRCLE_init(0, NULL);
 
-    CIRCLE_internal_queue_t *out_q;
+    CIRCLE_internal_queue_t* out_q;
     out_q = CIRCLE_internal_queue_init();
     CIRCLE_internal_queue_push(out_q, test_string);
 
-	checkpoint_write_ret = CIRCLE_internal_queue_write(out_q, fakerank);
+    checkpoint_write_ret = CIRCLE_internal_queue_write(out_q, fakerank);
     fail_unless(checkpoint_write_ret > 0, \
-	    "The checkpoint write function did not return a positive value.");
+                "The checkpoint write function did not return a positive value.");
 
     CIRCLE_internal_queue_free(out_q);
     CIRCLE_finalize();
 
-	CIRCLE_init(0, NULL);
-    CIRCLE_internal_queue_t *in_q;
+    CIRCLE_init(0, NULL);
+    CIRCLE_internal_queue_t* in_q;
     in_q = CIRCLE_internal_queue_init();
 
     checkpoint_read_ret = CIRCLE_internal_queue_read(in_q, fakerank);
-	fail_unless(checkpoint_read_ret > 0, \
-	    "The checkpoint read function did not return a positive value.");
+    fail_unless(checkpoint_read_ret > 0, \
+                "The checkpoint read function did not return a positive value.");
 
     CIRCLE_internal_queue_pop(in_q, result);
     fail_unless(in_q->count == 0, \
-        "Queue count was not correct after poping the last element.");
+                "Queue count was not correct after poping the last element.");
 
     fail_unless(strcmp(test_string, result) == 0, \
-        "Result poped from the queue does not match original.");
+                "Result poped from the queue does not match original.");
 
     CIRCLE_internal_queue_free(in_q);
     CIRCLE_finalize();
@@ -59,12 +59,12 @@ START_TEST
     fail();
 }
 END_TEST
- 
-Suite *
-check_checkpoint_suite (void)
+
+Suite*
+check_checkpoint_suite(void)
 {
-    Suite *s = suite_create("check_checkpoint");
-    TCase *tc_core = tcase_create("Core");
+    Suite* s = suite_create("check_checkpoint");
+    TCase* tc_core = tcase_create("Core");
 
     tcase_add_test(tc_core, test_checkpoint_single_read_write);
     tcase_add_test(tc_core, test_checkpoint_multiple_read_write);
@@ -76,12 +76,12 @@ check_checkpoint_suite (void)
 }
 
 int
-main (void)
+main(void)
 {
     int number_failed;
 
-    Suite *s = check_checkpoint_suite();
-    SRunner *sr = srunner_create(s);
+    Suite* s = check_checkpoint_suite();
+    SRunner* sr = srunner_create(s);
 
     srunner_run_all(sr, CK_NORMAL);
     number_failed = srunner_ntests_failed(sr);
