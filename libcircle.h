dcd9752ff92c3c78fa4a38daa9a5676b3c4ab199
Jharrod LaFon <jlafon@lanl.gov>
2012-03-01 20:22:39 -0700

    Removed locality awareness.

--- libcircle.h
+++ libcircle.h
@@ -18,7 +18,6 @@
   */
 #define CIRCLE_SPLIT_RANDOM     (1 << 0)              /* Split work randomly. */
 #define CIRCLE_SPLIT_EQUAL      ~CIRCLE_SPLIT_RANDOM  /* Split work evenly */
-#define CIRCLE_ENABLE_LOCALITY  (1 << 2)              /* Enable proximity awareness */
 #define CIRCLE_DEFAULT_FLAGS    CIRCLE_SPLIT_RANDOM   /* Default behavior is random work stealing */
 
 /**
