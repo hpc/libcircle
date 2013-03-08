3d7ac1bae7ba63dcf9b0de93e94b0c872d2bf7fa
Adam Moody <moody20@llnl.gov>
2013-03-08 10:52:27 -0800

    add void parameter to silence compiler warnings

--- libcircle/worker.h
+++ libcircle/worker.h
@@ -6,9 +6,9 @@
 
 #define LIBCIRCLE_MPI_ERROR 32
 
-int8_t CIRCLE_worker();
-int8_t _CIRCLE_read_restarts();
-int8_t _CIRCLE_checkpoint();
+int8_t CIRCLE_worker(void);
+int8_t _CIRCLE_read_restarts(void);
+int8_t _CIRCLE_checkpoint(void);
 void CIRCLE_reset_request_vector(CIRCLE_state_st* st);
 
 #endif /* WORKER_H */
